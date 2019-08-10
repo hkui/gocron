16b297bbb297668909f7cec9b7cad0927110db59
huangkui <huangkui@lepu.cn>
2019-08-10 19:00:19 +0800

    zc

--- base/login/common/types.go
+++ base/login/common/types.go
@@ -6,10 +6,5 @@ type Session interface {
 	Delete(key interface{}) error     //删除Session
 	SessionID() string                //当前SessionID
 }
-type Provider interface {
-	SessionInit(sid string) (Session, error)
-	SessionRead(sid string) (Session, error)
-	SessionDestroy(sid string) error
-	SessionGC(maxLifeTime int64)
-}
+
 
--- base/login/http.go
+++ base/login/http.go
@@ -1,31 +1,31 @@
 package main
 
 import (
-	"base/login/common"
 	"fmt"
+	"github.com/gorilla/sessions"
 	"net"
 	"net/http"
+
 )
 
 
-func main() {
+var store = sessions.NewFilesystemStore("./",[]byte("session_file"))
+//var store=sessions.NewCookieStore([]byte("session_cookie"))
 
+const sess_name  = "user"
+
+
+func main() {
 	var(
-		providers map[string]common.Provider
 		mux *http.ServeMux
 		listener net.Listener
 		err error
 		httpServer *http.Server
 	)
 
-	providers = make(map[string]common.Provider)
-
-
-
-
 
 	mux=http.NewServeMux()
-	mux.HandleFunc("/login",login)
+	mux.HandleFunc("/set",set)
 	mux.HandleFunc("/get",get)
 
 	if listener,err=net.Listen("tcp",":8888");err!=nil{
@@ -40,45 +40,67 @@ func main() {
 	}
 }
 
-func login(w http.ResponseWriter,  r *http.Request)  {
-	sess := G_session.SessionStart(w, r)
-	r.ParseForm()
-	name := sess.Get("username")
-	if name != nil {
-		sess.Set("username", r.Form["username"]) //将表单提交的username值设置到session中
-	}
-}
-
-func get(resp http.ResponseWriter,  r *http.Request)()  {
-	var (
-		cookie *http.Cookie
+func set(w http.ResponseWriter,  r *http.Request)  {
+	var(
 		err error
 		key string
+		value string
+		ses *sessions.Session
 	)
-	if err:=r.ParseForm();err!=nil{
-		fmt.Println(err)
+	err=r.ParseForm()
+	if(err!=nil){
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	key=r.Form.Get("key")
-
-	if cookie,err=r.Cookie(key);err!=nil{
+	value=r.Form.Get("value")
+	if len(key)<1 || len(value)<1{
+		http.Error(w,fmt.Errorf("参数缺失","参数缺失").Error(),http.StatusBadRequest)
+		return
+	}
+	if ses,err=store.Get(r,sess_name);err!=nil{
 		fmt.Println(err)
 		return
 	}
-	resp.Write([]byte(cookie.Value))
+	ses.Values[key]=value
+	ses.Options.MaxAge=20
 
+	// Save it before we write to the response/return from the handler.
+	err=ses.Save(r, w)
+	if err!=nil{
+		fmt.Println(err)
+	}else{
+		fmt.Printf("set %s=%s \n",key,value)
+	}
 }
-//注册一个能通过名称来获取的 session provider 管理器
-func RegisterProvider(name string, provider common.Provider) {
-	if provider == nil {
-		panic("session: Register provider is nil")
+
+func get(w http.ResponseWriter,  r *http.Request)()  {
+	var(
+		err error
+
+		out string
+		ses *sessions.Session
+	)
+	err=r.ParseForm()
+	if(err!=nil){
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
-	if _, p := providers[name]; p {
-		panic("session: Register provider is existed")
+	if ses,err=store.Get(r,sess_name);err!=nil{
+		fmt.Println(err)
+		return
 	}
 
-	providers[name] = provider
+
+	for k,v:=range ses.Values{
+		out+=fmt.Sprintf("%s=%s\n",k,v)
+	}
+	fmt.Println(out)
+	w.Write([]byte(out))
+
+
+
 }
 
 
