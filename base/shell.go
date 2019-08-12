a0a5b268d4aa302bc44e950fa73b0b714ea182a1
huangkui <huangkui@lepu.cn>
2019-08-12 19:28:08 +0800

    yii shell

--- base/shell.go
+++ base/shell.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"fmt"
 	"os/exec"
+	"strings"
 )
 
 func main() {
@@ -12,15 +13,21 @@ func main() {
 		cmd *exec.Cmd
 		output []byte
 		err error
+		stringout string
+		stringArr []string
 	)
 	//cmd=exec.Command("/bin/bash","-c","ls -al /")
-	cmd=exec.CommandContext(context.TODO(),"/bin/bash","-c","php /code/yii/yii")
+	cmd=exec.CommandContext(context.TODO(),"/bin/bash","-c","php /code/yii/yii|grep -E '[a-z-]+/[a-z-]+'|awk '{print $1}'")
 
 	if output,err=cmd.CombinedOutput();err!=nil{
 		fmt.Println(err)
 		return
 	}
+	stringout=string(output)
 
-	fmt.Println(string(output))
+	//fmt.Println(stringout)
+	stringArr=strings.Split(stringout,"\n")
+	fmt.Println(stringArr,len(stringArr))
+
 
 }
